0696687f8a84c2dbde9913ebea7091bb31145825
flaschd1 <david.flaschberger@edu.htl-villach.at>
2026-09-22 15:18:11 +0200

    fix: mobile scrolling + webuntis school search

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 */
 /* bump this on every deploy that changes index.html so phones actually
    pick up the update instead of serving a stale cached shell */
-const CACHE = 'lernio-v5';
+const CACHE = 'lernio-v6';
 const SHELL = [
   './',
   './index.html',
